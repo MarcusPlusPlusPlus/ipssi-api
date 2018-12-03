109beef4124ea742b64d628aac5f5e447dc33550
Allan Rethore <a.rethore@geolid.com>
2018-12-03 14:28:08 +0100

    Fix : Docker and add force dist file

---
+++ src/Controller/ArmoryController.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Controller;
+
+
+use App\Entity\Armory;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArmoryController extends AbstractController
+{
+    /**
+     * @Route(path="/armory", methods={"GET"})
+     *
+     * @return JsonResponse
+     */
+    public function list()
+    {
+        //Dependency
+        $armoryRepository = $this->getDoctrine()->getRepository(Armory::class);
+        $serailizer = $this->get('serializer');
+
+        //My Logic
+        $armoryContents = $armoryRepository->findAll();
+
+        //Send response
+        return new JsonResponse($serailizer->normalize($armoryContents), 200);
+    }
+}
